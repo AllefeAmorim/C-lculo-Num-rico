245461e5f825b8f469f7060698304b566049eda8
Állefe Amorim <143190917+AllefeAmorim@users.noreply.github.com>
2024-08-12 17:29:49 +0000

    Exercícios Resolvidos - aula 4 - Correção

--- 3/main.jl
+++ 3/main.jl
@@ -38,11 +38,61 @@ println(z)
 #Exercicio 3.2.8
 
 Ir = 10^-12
+T = 300
+k = 1.38*10^-23
+q = 1.602*10^-19
+I(x) = Ir*(exp((x*q) / (k*T)) - 1)
+g(x,V,R) = R*I(x) + x - V
+error = 10^-3
+
+
+#Exercicio 3.2.8
+#a)
 Vt = ((1.38*10^-23)*(300))/(1.60*10^-19)
+println(Vt)
+
 R = 1000
-V = 3
+V = 30
 f(x) = R*(Ir*(exp(x/Vt) - 1)) + x - V
 error = 10^-5
-a = 0
-b = 1
+a = 0.3
+b = 0.9
+println(bisection(a, b, f, error))
+#b)
+
+R = 1000
+V = 3
+println(bisection(a, b, f, error))
+
+#c)
+
+R = 10000
+V = 3
+println(bisection(a, b, f, error))
+
+#d)
+
+R = 10000
+V = 0.3
+a = 0.2
+b = 0.8
+println(bisection(a, b, f, error))
+
+#e)
+R = 1000
+V = -0.3
+a = -0.2
+b = -0.8
+println(bisection(a, b, f, error))
+
+#f)
+R = 1000
+V = -30
+a = -20
+b = -70
+println(bisection(a, b, f, error))
+
+#g)
+R = 10000
+V = -30
 println(bisection(a, b, f, error))
